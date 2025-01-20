e076b9ff479971261f0f7cc34ba85a8e5c840b0f
malufaccin <145558786+malufaccin@users.noreply.github.com>
2025-01-20 16:37:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taskForm = document.getElementById("task-form");
+    const tasksList = document.getElementById("tasks");
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach((task, index) => addTaskToDOM(task, index));
+
+    taskForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const title = document.getElementById("task-title").value;
+        const date = document.getElementById("task-date").value;
+        const description = document.getElementById("task-description").value;
+
+        const task = { title, date, description };
+        tasks.push(task);
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        addTaskToDOM(task, tasks.length - 1);
+
+        taskForm.reset();
+    });
+
+    function addTaskToDOM(task, index) {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <strong>${task.title}</strong> <br>
+            <em>${task.date}</em> <br>
+            ${task.description} <br>
+        `;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Deletar";
+        deleteButton.style.marginTop = "10px";
+        deleteButton.style.padding = "5px 10px";
+        deleteButton.style.border = "none";
+        deleteButton.style.backgroundColor = "#f890de";
+        deleteButton.style.color = "white";
+        deleteButton.style.cursor = "pointer";
+        deleteButton.addEventListener("click", () => deleteTask(index));
+
+        li.appendChild(deleteButton);
+        tasksList.appendChild(li);
+    }
+
+
+    function deleteTask(index) {
+
+        tasks.splice(index, 1);
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        renderTasks();
+    }
+
+    function renderTasks() {
+        tasksList.innerHTML = ""; 
+        tasks.forEach((task, index) => addTaskToDOM(task, index));
+    }
+});
